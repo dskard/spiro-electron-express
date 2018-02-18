fb6763a9094870647f58b698daefc8e2d428babb
dskard <telldsk@gmail.com>
2018-02-18 09:57:07 -0500

    start and stop ejs app from main.js
    add code to start and stop the web application server from the main.js
    file instead of trying to start it in javascript in the index.html.
    
    add logging that prints to stdout

--- main.js
+++ main.js
@@ -2,35 +2,106 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
+// Setup logging
+// Logs go to developer tools console and a file on disk
+const log = require('electron-log');
+
+log.transports.console.level = 'info';
+log.transports.file.level = 'info';
+
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 
-let win
+let mainWindow;
+let webServer;
+let shuttingDown;
+
+function startExpress() {
+    // Create the path of the express server to pass in with the spawn call
+    var webServerDirectory = path.join(__dirname, 'app', 'bin', 'www');
+    log.info('starting node script: ' + webServerDirectory);
+
+    // May need to change this if node is not in PATH
+    var nodePath = "node";
+
+    // Update environment variables
+    var env = JSON.parse(JSON.stringify(process.env));
+
+    // Start the node express server
+    const spawn = require('child_process').spawn;
+    webServer = spawn(nodePath,[webServerDirectory], {
+        env : env
+    });
+
+    // Check if the server started
+    if (!webServer) {
+        log.info("couldn't start web server");
+        return;
+    }
+
+    // Send stdout from child process to log
+    webServer.stdout.on('data', function (data) {
+        log.info('data: ' + data);
+    });
+
+    // Send child process messages to log
+    // triggered when child process uses process.send() to send messages.
+    webServer.on('message', function (message) {
+        log.info(message);
+    });
+
+    // Handle closing of the child process
+    webServer.on('close', function (code) {
+        log.info('child process exited with code ' + code);
+        webServer = null;
+
+        // Only restart if killed for a reason...
+        if (!shuttingDown) {
+            log.info('restarting...');
+            startExpress();
+        }
+    });
+
+    // Send stderr from child process to log
+    webServer.stderr.on('data', function (data) {
+        log.info('stderr: ' + data);
+    });
+
+    // Send errors from child process to log
+    // things like:
+    // process could not be spawned
+    // process could not be killed
+    webServer.on('error', function (err) {
+        log.info('web server error: ' + err);
+    });
+} // end startExpress
+
 
 function createWindow() {
     // Create the browser window
-    win = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         autoHideMenuBar: true,
         width: 1000,
         height: 630
     })
 
     // and load the index.html of the app.
-    win.loadURL(url.format({
+    mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
 
     // Open the DevTools.
-    //win.webContents.openDevTools()
+    //mainWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
-    win.on('closed', () => {
+    mainWindow.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        win = null
+        mainWindow = null
     })
 
 } // end createWindow
@@ -39,12 +110,29 @@ function createWindow() {
 // This method will be called when Elextron has finished
 // initialization and is read to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+    shuttingDown = false;
+    startExpress();
+    createWindow();
+});
 
 
 //app.on("browser-window-created", function(e,window) {
 //    window.setMenu(null);
 //})
+
+
+// Called before quitting
+// Gives us a chance to shut down the child process
+app.on('before-quit', function () {
+    log.info('gracefully shutting down...');
+
+    // flag to know that we are shutting down
+    shuttingDown = true;
+
+    // kill the web server
+    webServer.kill();
+});
 
 
 // Quit when all windows are closed.
@@ -57,10 +145,17 @@ app.on('window-all-closed', () => {
 })
 
 
+process.on ("SIGINT", function () {
+    // graceful shutdown
+    log.info('shutting down...');
+    process.exit();
+});
+
+
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if  (win === null ) {
+    if  (mainWindow === null ) {
         createWindow()
     }
 })
